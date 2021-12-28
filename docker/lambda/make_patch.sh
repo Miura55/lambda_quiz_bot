a2221430a2cff2807af3cb43b0d02c79b367b905
Miura55 <acordion.piano@gmail.com>
2021-12-28 13:01:18 +0900

    create mst for model

--- docker/lambda/make_patch.sh
+++
@@ -1,8 +0,0 @@
-# パッチファイルがあれば削除する
-if [ -e lambda_patch.zip ]; then
-    rm lambda_patch.zip
-    echo "Removed old lambda_patch.zip"
-fi
-
-# buildディレクトリ内のファイルをzipに圧縮して保存する
-zip -r lambda_patch build/
